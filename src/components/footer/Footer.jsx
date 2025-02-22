19015ae41faab5f8522f2dae79d0376fc121b784
TmrBotz <tmrbotz@gmail.com>
2025-02-22 15:46:12 +0530

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,40 +8,13 @@ function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.openWeather}>
-        <p>Powered by </p>
-        <a
-          href="https://openweathermap.org/api"
-          title="Free OpenWeather Api"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={openWeatherLogo} alt="openWeather" loading="lazy" />
-        </a>
-      </div>
-      <p className={styles.info}>Designed and Coded by :</p>
+        
+      <p className={styles.info}>Designed and Coded by : @Tmr_botz</p>
       <p>
         <sup>&#169; </sup>
-        {currentYear} <span className={styles.myName}>Abdelrahman Alsayed</span>
+        {currentYear} <span className={styles.myName}>Tmrbotz</span>
       </p>
-      <div className={styles.links}>
-        <a
-          href="https://www.linkedin.com/in/abdelrahman-alsayed-b312311a0/"
-          title="Author LinkedIn account"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FaLinkedin />
-        </a>
-        <a
-          href="https://github.com/AbdelrahmanAlsayed"
-          title="Author github account"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FaGithub />
-        </a>
-      </div>
-    </footer>
+      </footer>
   );
 }
 
